bb45b0b1ef8b8e4c6a7dec1b044d79642bf47b09
zubimass007 <99318118+zubimass007@users.noreply.github.com>
2025-09-18 12:47:23 +0530

    github link test

---
+++ test.js
@@ -0,0 +1,15 @@
+// filename: test.js
+
+// A simple function to add two numbers
+function add(a, b) {
+  return a + b;
+}
+
+// A simple function to multiply two numbers
+function multiply(a, b) {
+  return a * b;
+}
+
+// Run some tests
+console.log("2 + 3 =", add(2, 3));
+console.log("4 * 5 =", multiply(4, 5));
